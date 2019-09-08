e12e14bc4d8635e1ea72775bc3798ceb12d3c950
wutao <wutao@qq.com>
2019-09-08 19:02:21 +0800

    更新类库

---
+++ mod/container/gvar/type.go
@@ -0,0 +1,36 @@
+package gvar
+
+// Frequently-used map type alias.
+type Map = map[string]interface{}
+type MapAnyAny = map[interface{}]interface{}
+type MapAnyStr = map[interface{}]string
+type MapAnyInt = map[interface{}]int
+type MapStrAny = map[string]interface{}
+type MapStrStr = map[string]string
+type MapStrInt = map[string]int
+type MapIntAny = map[int]interface{}
+type MapIntStr = map[int]string
+type MapIntInt = map[int]int
+
+// Frequently-used slice type alias.
+type List = []Map
+type ListAnyStr = []map[interface{}]string
+type ListAnyInt = []map[interface{}]int
+type ListStrAny = []map[string]interface{}
+type ListStrStr = []map[string]string
+type ListStrInt = []map[string]int
+type ListIntAny = []map[int]interface{}
+type ListIntStr = []map[int]string
+type ListIntInt = []map[int]int
+
+// Frequently-used slice type alias.
+type Slice = []interface{}
+type SliceAny = []interface{}
+type SliceStr = []string
+type SliceInt = []int
+
+// Array is alias of Slice.
+type Array = []interface{}
+type ArrayAny = []interface{}
+type ArrayStr = []string
+type ArrayInt = []int
